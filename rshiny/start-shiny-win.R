4360cce9942e6276b20a7fc205805a387f9a0f27
Ippei Akiya <ippei.aki@datadriven.jp>
2021-01-11 16:56:08 +0900

    update

--- rshiny/start-shiny-win.R
+++ rshiny/start-shiny-win.R
@@ -1,16 +1,12 @@
 # Script that starts the shiny webserver
 # Parameters are supplied using environment variables
 
-.libPaths("C:/Users/ippei/Documents/R/win-library/4.0")
-#.libPaths( Sys.getenv()['R_LIBS_USER'][[1]] )
-
-options(shiny.reactlog=TRUE)
+#.libPaths("C:/Users/ippei/Documents/R/win-library/4.0")
+.libPaths( Sys.getenv()['R_LIBS_USER'][[1]] )
 
 shiny::runApp(
   paste0("C:/Users/", Sys.info()[["user"]], "/.douhau-data-studio/rshiny/app"),
   host = "127.0.0.1",
   launch.browser = FALSE,
-  port = 8181,
-  display.mode="showcase"
+  port = 8181
 )
-
